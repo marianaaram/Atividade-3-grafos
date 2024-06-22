11e6cdebb0587963b3caf8d668d07bb4ec18f849
Mariana Aram <marianaaram8@gmail.com>
2024-06-22 02:39:42 -0300

    Parte 1
    Falta colocar o resto dos metodos no main e verificar se o algoritmo case 6 está funcionando corretamente

---
+++ TRABALGO-3-GRAFOS/src/LimparTela.java
@@ -0,0 +1,12 @@
+public class LimparTela {
+    
+    public static void limpar_console () throws Exception {
+        //Limpa a tela no windows, no linux e no MacOS
+        if (System.getProperty("os.name").contains("Windows"))
+            new ProcessBuilder("cmd", "/c", "cls").inheritIO().start().waitFor();
+        else
+            Runtime.getRuntime().exec("clear");
+    }
+
+    
+}
